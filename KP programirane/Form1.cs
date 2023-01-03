8ed6ba38773bd2f5f02db4f441c1f817968e4184
Константин Симеонов <ks08795672@edu.mon.bg>
2023-01-04 00:21:19 +0300

    Triangle button

--- KP programirane/Form1.cs
+++ KP programirane/Form1.cs
@@ -28,6 +28,37 @@ namespace KP_programirane
                 Thread.Sleep(3000);
             }
         }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
 
+            Thread t = new Thread(Triangle);
+            t.Start();
+        }
+
+        private void Triangle()
+        {
+            for (int i = 0; i < 50; i--)
+            {
+                Random rnd = new Random();
+                int size = rnd.Next(50, 200);
+                int x = rnd.Next(0, this.ClientSize.Width - size);
+                int y = rnd.Next(0, this.ClientSize.Height - size);
+
+                using (Graphics g = this.CreateGraphics())
+                {
+                    Point[] points = {
+            new Point(x, y),
+            new Point(x + size, y),
+            new Point(x + size / 2, y + size) };
+
+                    Pen penpen = new Pen(Brushes.Black);
+                    g.DrawPolygon(penpen, points);
+                }
+                Thread.Sleep(4000);
+            }
+
+        }
+
+       
     }
 }
